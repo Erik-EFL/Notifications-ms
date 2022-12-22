00a73cc90a780bbaaa2bb6fba6ed6a69070141c5
erik-efl <erik.lima.f@outlook.com>
2022-12-22 19:39:57 -0300

    feat: :sparkles: Addin a new prop
    ValidationPipe
    
    responsible for adding a validations pipe

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); // <== Adiciona o pipe de validação
+  await app.listen(3000);
+}
+bootstrap();
