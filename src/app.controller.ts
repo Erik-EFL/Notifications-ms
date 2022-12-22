8dac5c328f25c7b8247b938193ab7af3df3d2883
erik-efl <erik.lima.f@outlook.com>
2022-12-22 19:37:58 -0300

    feat: :sparkles: Adding a new method
    Create post method
    
    responsible for create a new notifications

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,28 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './Notification.body';
 import { PrismaService } from './prisma.service';
+
 @Controller('notifications') // <== define o prefixo da rota
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
+
   @Get()
   list() {
     return this.prisma.notification.findMany();
   }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    return this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
